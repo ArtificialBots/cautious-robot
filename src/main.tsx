6da908ee207a99a2abada77e58c97b79faa741dd
ArtificialBots <121661693+ArtificialBots@users.noreply.github.com>
2025-09-17 11:33:11 +1000

    Fix Tailwind, Ace Editor, and JSX errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ace from 'ace-builds'
 import App from './App.tsx'
 import './index.css'
+
+// Configure Ace Editor base path
+ace.config.set('basePath', '/node_modules/ace-builds/src-noconflict')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
